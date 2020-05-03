0be10d41c022575f584bd84914b57bce7d874e90
sopapado <sotocpa@gmail.com>
2020-05-03 20:06:50 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='photonic_sim',
+      version='1.0',
+      description='A collection of methods simulating propagation of optical waves in dielectric or lossy materials with 1D geometry',
+      url='http://github.com/sopapado/photonic_sim',
+      author='Sotirios Papadopoulos',
+      author_email='send@git.com',
+      license='MIT',
+      packages=['photonic_sim'],
+      zip_safe=False)
+      
