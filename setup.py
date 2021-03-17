fe2d38309264c1cd0e69995fb8552b994b4b1f98
sopapado <sotocpa@gmail.com>
2021-03-17 14:18:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='photonic_sim',
       author_email='send@git.com',
       license='MIT',
       packages=['photonic_sim'],
-      install_requires = [ 'tabulate','tqdm','addcopyfighandler','mpmath','pickle-mixin','w2rgb'],
+      install_requires = [ 'tabulate','tqdm','addcopyfighandler','mpmath','pickle-mixin'],
       zip_safe=False)
       
 
